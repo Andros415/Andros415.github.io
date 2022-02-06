aecda2bc4be63c86090d0e249020df64810da400
Andrei Spatariu <andros415@gmail.com>
2022-02-06 15:12:21 -0500

    Adjusted title gradient and customized scrollbar

--- src/components/Splash/Splash.tsx
+++ src/components/Splash/Splash.tsx
@@ -2,9 +2,9 @@ import React, { ReactElement } from "react";
 
 function Splash(): ReactElement {
   return (
-    <div className="min-h-screen min-w-full flex items-center justify-center">
-      <div className="text-white text-center space-y-7">
-        <h1 className="bg-clip-text bg-gradient-to-r from-[#61F4DE] to-[#6E78FF] text-transparent font-bold font-main text-7xl tracking-wider">
+    <div className="flex min-h-screen min-w-full items-center justify-center">
+      <div className="space-y-7 text-center text-white">
+        <h1 className="bg-gradient-to-r from-[#61F4DE] to-[#64ACFF] bg-clip-text font-main text-7xl font-bold tracking-wider text-transparent">
           Andrei Spatariu
         </h1>
         <p className="font-handwriting text-5xl">Full-stack Web Developer</p>
